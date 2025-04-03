359dfed40089a956cc35b94fd35d1e7873aa0376
Andrew Igure <andrewoigure@gmail.com>
2025-04-03 14:35:31 +0100

    Update CORS configuration to allow Vercel domain and local origins

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 // Configure CORS to allow requests from your Vercel domain and local development.
 const allowedOrigins = [
   "https://vistapro-4xlusoclj-vistapros-projects.vercel.app",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://www.vistapro.ng"
 ];
 
 app.use(cors({
