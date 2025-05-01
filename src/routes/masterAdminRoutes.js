687b4db62b0fdb877a41cc845afdb0e5c56a30d6
Andrew Igure <andrewoigure@gmail.com>
2025-05-01 21:47:34 +0100

    fix(verification): accept marketerUniqueId in DELETE submissions and emit formReset

--- src/routes/masterAdminRoutes.js
+++ src/routes/masterAdminRoutes.js
@@ -111,7 +111,7 @@ router.post(
 router.get('/dashboard-summary', verifyToken, verifyRole(['MasterAdmin']), getDashboardSummary);
 router.get('/users/summary', verifyToken, verifyRole(['MasterAdmin']), getUserSummary);
 router.put('/users/:id', verifyToken, verifyRole(['MasterAdmin']), updateUser);
-router.delete('/users/:id', verifyToken, verifyRole(['MasterAdmin']), deleteUser);
+router.delete('/users/:uniqueId', verifyToken, verifyRole(['MasterAdmin']), deleteUser);
 router.patch('/users/:id/lock', verifyToken, verifyRole(['MasterAdmin']), lockUser);
 router.patch('/users/:id/unlock', verifyToken, verifyRole(['MasterAdmin']), unlockUser);
 
